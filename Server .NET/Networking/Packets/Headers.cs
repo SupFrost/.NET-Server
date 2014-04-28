f323007f224e6c7b0d1fdea79c73b5e1778f1721
SupFrost - Nick Jorens <nickjorens@gmail.com>
2014-04-28 21:23:29 +0200

    Included untracked file

---
+++ Server .NET/Networking/Packets/Headers.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Networking.Packets
+{
+    public enum IoHeader : ushort
+    {
+       Request,
+       Send
+    }
+
+    public enum StandardHeader : ushort
+    {
+        Guid,
+        Ping
+    }
+}
